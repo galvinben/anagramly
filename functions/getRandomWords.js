d3c2e9aaddf400a4dc9e0bfb824e9578ade80e50
galvinben <galvin.ben@protonmail.com>
2020-04-08 10:59:01 +0100

    try env var

--- functions/getRandomWords.js
+++ functions/getRandomWords.js
@@ -28,7 +28,7 @@ exports.handler = async (event, context) => {
       }
     }
     console.log(freshWords)
-    return { statusCode: 200, body: freshWords[0].word }
+    return { statusCode: 200, body: process.env.WORDS_API_URL }
   } catch (e) {
     this.error = e.response
     this.response = '—'
