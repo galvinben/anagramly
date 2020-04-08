8e4736829abc4bce515b17c713d102bb27f92745
galvinben <galvin.ben@protonmail.com>
2020-04-08 11:12:52 +0100

    logging

--- functions/getRandomWords.js
+++ functions/getRandomWords.js
@@ -1,9 +1,13 @@
 exports.handler = async (event, context) => {
   let min = event.queryStringParameters.min
   let max = event.queryStringParameters.max
-
   let wordCount = event.queryStringParameters.wordCount
-
+  console.log(min)
+  console.log(max)
+  console.log(wordCount)
+  console.log(process.env.WORDS_API_URL)
+  console.log(process.env.WORDS_API_HOST)
+  console.log(process.env.WORDS_API_KEY)
   try {
     let params =
       max > 0 ? `&lettersMin=${min}&lettersMax=${max}` : `&lettersMin=${min}`
@@ -17,7 +21,7 @@ exports.handler = async (event, context) => {
           },
         })
         .then(function (response) {
-          console.log(resonse)
+          console.log(response)
           return response.data
         })
         .catch(function (error) {
