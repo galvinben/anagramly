7043862c0e6acfbb16775ce248dc6201da1dcc3b
galvinben <galvin.ben@protonmail.com>
2020-04-08 10:52:37 +0100

    try returning string

--- functions/getRandomWords.js
+++ functions/getRandomWords.js
@@ -28,7 +28,7 @@ exports.handler = async (event, context) => {
     //   }
     // }
     console.log(freshWords)
-    return { statusCode: 200, body: { words: freshWords } }
+    return { statusCode: 200, body: 'asd' }
   } catch (e) {
     this.error = e.response
     this.response = '—'
