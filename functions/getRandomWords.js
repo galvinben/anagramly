7ab2dde5407842de3c1fe13355beecec7738349d
galvinben <galvin.ben@protonmail.com>
2020-04-08 10:48:37 +0100

    remove words api to test

--- functions/getRandomWords.js
+++ functions/getRandomWords.js
@@ -7,26 +7,26 @@ exports.handler = async (event, context) => {
   try {
     let params =
       max > 0 ? `&lettersMin=${min}&lettersMax=${max}` : `&lettersMin=${min}`
-    let freshWords = []
-    while (freshWords.length < wordCount) {
-      let data = await axios
-        .get(`${process.env.WORDS_API_URL}{params}`, {
-          headers: {
-            'x-rapidapi-host': process.env.WORDS_API_HOST,
-            'x-rapidapi-key': process.env.WORDS_API_KEY,
-          },
-        })
-        .then(function (response) {
-          console.log(resonse)
-          return response.data
-        })
-        .catch(function (error) {
-          console.log(error)
-        })
-      if (!data.word.includes(' ')) {
-        freshWords.push(data)
-      }
-    }
+    let freshWords = [{ word: 'test', results: [{ definition: 'test' }] }]
+    // while (freshWords.length < wordCount) {
+    //   let data = await axios
+    //     .get(`${process.env.WORDS_API_URL}{params}`, {
+    //       headers: {
+    //         'x-rapidapi-host': process.env.WORDS_API_HOST,
+    //         'x-rapidapi-key': process.env.WORDS_API_KEY,
+    //       },
+    //     })
+    //     .then(function (response) {
+    //       console.log(resonse)
+    //       return response.data
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error)
+    //     })
+    //   if (!data.word.includes(' ')) {
+    //     freshWords.push(data)
+    //   }
+    // }
     console.log(freshWords)
     return { statusCode: 200, body: { words: freshWords } }
   } catch (e) {
